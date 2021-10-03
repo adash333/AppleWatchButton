ca4a101ff2229689e8e776ca19533ea9cf2abd70
“adash333” <adash333@hotmail.com>
2021-10-04 00:01:17 +0900

    Initial Commit

---
+++ Button WatchKit Extension/ButtonApp.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonApp.swift
+//  Button WatchKit Extension
+//
+//  Created by a on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
